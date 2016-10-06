06f6ad6b9fdfcf0fa99920198bc4bfca66072248
ganyao114 <939543405@qq.com>
2016-10-06 09:21:57 +0800

    架构完成,ActivityLife模块完成

--- app/src/main/java/net/swiftos/eventtest/MainActivity.java
+++ app/src/main/java/net/swiftos/eventtest/MainActivity.java
@@ -12,16 +12,25 @@ import net.swiftos.eventposter.Impls.ActivityLife.Handler.ActivityLifeHandler;
 
 public class MainActivity extends AppCompatActivity {
 
+    public MainActivity() {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EventPoster.Regist(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EventPoster.Regist(this);
     }
 
     @ActivityLife(lifeType = ActivityLifeType.OnPause,activity = MainActivity.class)
     public void onPause(Activity activity){
         Toast.makeText(activity,"pause",Toast.LENGTH_LONG).show();
+    }
+
+    @ActivityLife(lifeType = ActivityLifeType.OnCreate,activity = MainActivity.class)
+    public void onCreate(Activity activity,Bundle bundle){
+        Toast.makeText(activity,"create",Toast.LENGTH_LONG).show();
     }
 
     @Override
---
+++ eventposter/src/main/java/net/swiftos/eventposter/Invoker/ProxyEntity.java
@@ -0,0 +1,37 @@
+package net.swiftos.eventposter.Invoker;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by pc on 16/5/14.
+ */
+public class ProxyEntity implements Runnable {
+
+    private Method method;
+    private Object object;
+    private Object[] args;
+    private Object ret;
+    private int delay = 0;
+
+    public ProxyEntity(Method method, Object object, Object... args) {
+        this.method = method;
+        this.object = object;
+        this.args = args;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+
+    @Override
+    public void run() {
+        if (delay!=0){
+            try {
+                Thread.currentThread().sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Invoker.invoke_direct(method,object,args);
+    }
+}
