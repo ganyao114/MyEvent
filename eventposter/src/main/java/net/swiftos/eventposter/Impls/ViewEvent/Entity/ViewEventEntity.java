64329d5764abf606165630b748e2b1dfaf6dd73b
ganyao114 <939543405@qq.com>
2016-10-10 17:22:59 +0800

    add viewevent

--- eventposter/src/main/java/net/swiftos/eventposter/Impls/ViewEvent/Entity/ViewEventEntity.java
+++ eventposter/src/main/java/net/swiftos/eventposter/Impls/ViewEvent/Entity/ViewEventEntity.java
@@ -3,10 +3,51 @@ package net.swiftos.eventposter.Impls.ViewEvent.Entity;
 import net.swiftos.eventposter.Exception.EventInvokeException;
 import net.swiftos.eventposter.Interface.IEventEntity;
 
+import java.lang.reflect.Method;
+
 /**
  * Created by gy939 on 2016/10/10.
  */
 public class ViewEventEntity implements IEventEntity{
+
+    private int[] ids;
+    private Method registMethod;
+    private Method callBackMethod;
+    private Class inter;
+
+
+    public int[] getIds() {
+        return ids;
+    }
+
+    public void setIds(int[] ids) {
+        this.ids = ids;
+    }
+
+    public Method getRegistMethod() {
+        return registMethod;
+    }
+
+    public void setRegistMethod(Method registMethod) {
+        this.registMethod = registMethod;
+    }
+
+    public Method getCallBackMethod() {
+        return callBackMethod;
+    }
+
+    public void setCallBackMethod(Method callBackMethod) {
+        this.callBackMethod = callBackMethod;
+    }
+
+    public Class getInter() {
+        return inter;
+    }
+
+    public void setInter(Class inter) {
+        this.inter = inter;
+    }
+
     @Override
     public Object invoke(Object invoker, Object... pars) throws EventInvokeException {
         return null;
--- eventposter/src/main/java/net/swiftos/eventposter/Impls/ViewEvent/Handler/ViewEventHandler.java
+++ eventposter/src/main/java/net/swiftos/eventposter/Impls/ViewEvent/Handler/ViewEventHandler.java
@@ -1,13 +1,27 @@
 package net.swiftos.eventposter.Impls.ViewEvent.Handler;
 
+import android.util.SparseArray;
+import android.view.View;
+
 import net.swiftos.eventposter.Entity.EventAnnoInfo;
 import net.swiftos.eventposter.Impls.ViewEvent.Entity.ViewEventEntity;
 import net.swiftos.eventposter.Interface.IHandler;
+
+import java.lang.annotation.Annotation;
+import java.lang.ref.WeakReference;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by gy939 on 2016/10/10.
  */
 public class ViewEventHandler implements IHandler<ViewEventEntity> {
+
+    private Map<Integer,WeakReference<View>> viewMap = new ConcurrentHashMap<>();
+
     @Override
     public void init(Object... objects) {
 
@@ -20,6 +34,11 @@ public class ViewEventHandler implements IHandler<ViewEventEntity> {
 
     @Override
     public ViewEventEntity parse(EventAnnoInfo annoInfo) {
+        Annotation annotation = annoInfo.getAnnotation();
+        int[] ids = getIdFromAnno(annotation);
+        if (ids == null)
+            return null;
+
         return null;
     }
 
@@ -38,8 +57,31 @@ public class ViewEventHandler implements IHandler<ViewEventEntity> {
 
     }
 
+    private int[] getIdFromAnno(Annotation annotation){
+        int[] viewIds = null;
+        try {
+            Method aMethod = annotation.annotationType()
+                    .getDeclaredMethod("value");
+            viewIds = (int[]) aMethod
+                    .invoke(annotation);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return viewIds;
+    }
+
     @Override
     public void remove(Object object) {
 
     }
+
+    public void addView(int viewId,View view){
+
+    }
+
+    public void removeView(int viewId){
+
+    }
+
+
 }
